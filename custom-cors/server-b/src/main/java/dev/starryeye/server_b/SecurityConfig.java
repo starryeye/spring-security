69956410e7d5185e1e7e15b4092412a118df04d8
starryeye <see2818@naver.com>
2024-09-26 23:48:07 +0900

    [starryeye, 2024.09.26] - Custom CORS
    내용
    - Custom CORS

--- custom-cors/server-b/src/main/java/dev/starryeye/server_b/SecurityConfig.java
+++ custom-cors/server-b/src/main/java/dev/starryeye/server_b/SecurityConfig.java
@@ -14,6 +14,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        /**
+         * Spring Security 의 CORS 기능의 관련 필터는
+         * org.springframework.web.filter 의 CorsFilter 이다.
+         *
+         * CorsFilter 는 Spring Security 의 인증 관련 필터들 보다 먼저 수행된다.
+         * - 사전 요청이면 다음 필터로 넘기지 않고 적절한 응답 셋팅 후에 바로 client 로 응답해버린다.
+         * - simple request, preflight request 방법 모두에 대해.. isValid 를 수행해보는데..
+         *      웹 브라우저가 수행할 일을 서버에서 수행해버리는 듯..
+         *          요청의 orgin 헤더 값 과 해당 B 서버의 CORS 셋팅을 비교해서 일치되지 않으면 다음 필터로 넘기지 않는듯..
+         */
+
         http.authorizeHttpRequests(auth ->
                 auth.anyRequest().permitAll() // 모든 요청을 인증 없이 허용
         )
@@ -30,6 +41,8 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
+        // http://localhost:8080 (스키마+호스트+포트) 가 모두 동일해야 동일 출처, 셋 중 하나라도 다르게 사용한다면 ...
+        // -> 웹 브라우저는.. simple request 라면 해당 응답을 사용자에게 노출하지 않고, preflight request 라면 본 요청은 수행하지 않는다.
         configuration.addAllowedOrigin("http://localhost:8080");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
