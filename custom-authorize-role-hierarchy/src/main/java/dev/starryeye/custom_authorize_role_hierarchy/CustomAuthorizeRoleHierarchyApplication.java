371e758d8db97f417c89f11985de2cb5435c48dd
starryeye <see2818@naver.com>
2024-10-07 22:10:21 +0900

    [starryeye, 2024.10.07] - Custom Authorize, RoleHierarchy
    내용
    - Custom Authorize, RoleHierarchy

---
+++ custom-authorize-role-hierarchy/src/main/java/dev/starryeye/custom_authorize_role_hierarchy/CustomAuthorizeRoleHierarchyApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_authorize_role_hierarchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthorizeRoleHierarchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAuthorizeRoleHierarchyApplication.class, args);
+	}
+
+}
---
+++ custom-authorize-role-hierarchy/src/test/java/dev/starryeye/custom_authorize_role_hierarchy/CustomAuthorizeRoleHierarchyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_authorize_role_hierarchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAuthorizeRoleHierarchyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
