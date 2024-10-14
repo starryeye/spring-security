12c2105f11026236bb677c6794c8f7566220fc83
starryeye <see2818@naver.com>
2024-10-14 22:21:57 +0900

    [starryeye, 2024.10.14] - Custom Authorize, MethodInterceptor (feat. 메서드 기반 AOP)
    내용
    - Custom Authorize, MethodInterceptor (feat. 메서드 기반 AOP)

---
+++ custom-authorize-method-interceptor/src/main/java/dev/starryeye/custom_authorize_method_interceptor/config/CustomMethodInterceptor.java
@@ -0,0 +1,42 @@
+package dev.starryeye.custom_authorize_method_interceptor.config;
+
+import lombok.RequiredArgsConstructor;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@RequiredArgsConstructor
+public class CustomMethodInterceptor implements MethodInterceptor {
+
+    /**
+     * 커스텀 MethodInterceptor (Advice) 를 만들어본다.
+     *
+     * AuthorizationManagerBeforeMethodInterceptor 는 AuthorizationAdvisor 를 구현하며
+     * AuthorizationAdvisor 는 MethodInterceptor 를 상속한다.
+     */
+
+    private final AuthorizationManager<MethodInvocation> authorizationManager;
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+
+        // SecurityContextHolder 에 존재하는 인증 객체 얻기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 주입된 AuthorizationManager 로 권한 심사를 진행
+        boolean granted = authorizationManager.check(
+                () -> authentication,
+                invocation
+        ).isGranted();
+
+        // 권한 심사 결과에 따라 원본 객체 수행 or AccessDeniedException
+        if (granted) {
+            return invocation.proceed();
+        }
+
+        throw new AccessDeniedException("Access denied");
+    }
+}
--- custom-authorize-method-interceptor/src/main/java/dev/starryeye/custom_authorize_method_interceptor/config/MethodSecurityConfig.java
+++ custom-authorize-method-interceptor/src/main/java/dev/starryeye/custom_authorize_method_interceptor/config/MethodSecurityConfig.java
@@ -2,8 +2,5 @@ package dev.starryeye.custom_authorize_method_interceptor.config;
 
 public class MethodSecurityConfig {
 
-    /**
-     * AuthorizationManagerBeforeMethodInterceptor 는 AuthorizationAdvisor 를 구현하며
-     * AuthorizationAdvisor 는 MethodInterceptor 를 상속한다.
-     */
+
 }
