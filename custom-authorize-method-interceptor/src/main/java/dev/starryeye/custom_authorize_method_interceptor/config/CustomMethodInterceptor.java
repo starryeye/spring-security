233576f4a518dc6334559df17a9e61d75f2fad7a
starryeye <see2818@naver.com>
2024-10-15 23:03:58 +0900

    [starryeye, 2024.10.15] - Custom Authorize, MethodInterceptor (feat. 메서드 기반 AOP)
    내용
    - Custom Authorize, MethodInterceptor (feat. 메서드 기반 AOP)

--- custom-authorize-method-interceptor/src/main/java/dev/starryeye/custom_authorize_method_interceptor/config/CustomMethodInterceptor.java
+++ custom-authorize-method-interceptor/src/main/java/dev/starryeye/custom_authorize_method_interceptor/config/CustomMethodInterceptor.java
@@ -16,15 +16,17 @@ public class CustomMethodInterceptor implements MethodInterceptor {
      *
      * AuthorizationManagerBeforeMethodInterceptor 는 AuthorizationAdvisor 를 구현하며
      * AuthorizationAdvisor 는 MethodInterceptor 를 상속한다.
+     * MethodInterceptor 는 최종적으로 Advice 를 상속한다.
      */
 
+    // 권한을 심사할 AuthorizationManager 를 주입 받는다.
     private final AuthorizationManager<MethodInvocation> authorizationManager;
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
 
         // SecurityContextHolder 에 존재하는 인증 객체 얻기
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
 
         // 주입된 AuthorizationManager 로 권한 심사를 진행
         boolean granted = authorizationManager.check(
--- custom-authorize-method-interceptor/src/main/java/dev/starryeye/custom_authorize_method_interceptor/config/MethodSecurityConfig.java
+++ custom-authorize-method-interceptor/src/main/java/dev/starryeye/custom_authorize_method_interceptor/config/MethodSecurityConfig.java
@@ -1,6 +1,42 @@
 package dev.starryeye.custom_authorize_method_interceptor.config;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = false) // PreAuthorize, PostAuthorize, PreFilter, PostFilter 비활성화
 public class MethodSecurityConfig {
 
+    @Bean
+    public Advisor helloServiceAdvisor() { // Advisor
 
+        // Spring AOP 가 기본적으로 제공하는 Advisor 생성자
+        return new DefaultPointcutAdvisor(pointcut(), methodInterceptor());
+    }
+
+    private MethodInterceptor methodInterceptor() { // Advice
+
+        // 인증 여부를 심사하는 AuthorizationManager
+        AuthorizationManager<MethodInvocation> authorizationManager = new AuthenticatedAuthorizationManager<>();
+
+        // 커스텀 MethodInterceptor (Advice) 생성
+        return new CustomMethodInterceptor(authorizationManager);
+    }
+
+    private Pointcut pointcut() { // Pointcut
+
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* dev.starryeye.custom_authorize_method_interceptor.service.HelloService.*(..))");
+
+        return pointcut;
+    }
 }
