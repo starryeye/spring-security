a9638930089363a314c8ec4343030a86225d4aab
starryeye <see2818@naver.com>
2024-12-13 22:22:20 +0900

    [starryeye, 2024.12.13] - Custom Authenticate, custom MVC Integration
    내용
    - Custom Authenticate, custom MVC Integration

---
+++ custom-authenticate-mvc-integration/src/main/java/dev/starryeye/custom_authenticate_mvc_integration/CustomAuthenticateMvcIntegrationApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_authenticate_mvc_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthenticateMvcIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAuthenticateMvcIntegrationApplication.class, args);
+	}
+
+}
