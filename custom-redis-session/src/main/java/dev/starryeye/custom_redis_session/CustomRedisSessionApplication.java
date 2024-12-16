dbb543be791c86f818cd9a4b2b856ed6819fa440
starryeye <see2818@naver.com>
2024-12-17 00:06:15 +0900

    [starryeye, 2024.12.17] - Custom Session store with Redis
    내용
    - Custom Session store with Redis

---
+++ custom-redis-session/src/main/java/dev/starryeye/custom_redis_session/CustomRedisSessionApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_redis_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRedisSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomRedisSessionApplication.class, args);
+	}
+
+}
---
+++ custom-redis-session/src/test/java/dev/starryeye/custom_redis_session/CustomRedisSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_redis_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRedisSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
