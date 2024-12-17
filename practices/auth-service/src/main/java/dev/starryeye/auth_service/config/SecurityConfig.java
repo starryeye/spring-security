44e1a911602f967fc7f012de9f9ce1e8ea55b825
starryeye <see2818@naver.com>
2024-12-18 04:04:09 +0900

    [starryeye, 2024.12.18] - 실습, 기본 SecurityFilterChain, UserDetailsService 생성
    내용
    - 실습, 기본 SecurityFilterChain, UserDetailsService 생성

---
+++ practices/auth-service/src/main/java/dev/starryeye/auth_service/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package dev.starryeye.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- practices/auth-service/src/main/java/dev/starryeye/auth_service/controller/HomeController.java
+++ practices/auth-service/src/main/java/dev/starryeye/auth_service/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String dashboard() {
-        return "/dashboard";
+    public String home() {
+        return "/home";
     }
 
     @GetMapping("/user")
