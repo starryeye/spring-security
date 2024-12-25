49e98e8f939a354b2f6c9aeefe2271b52c8cddc7
starryeye <see2818@naver.com>
2024-12-25 18:52:16 +0900

    [starryeye, 2024.12.25] - 실습, custom AuthenticationSuccessHandler
    내용
    - 실습, custom AuthenticationSuccessHandler

--- practices/auth-service/src/main/java/dev/starryeye/auth_service/security/MyAuthenticationSuccessHandler.java
+++ practices/auth-service/src/main/java/dev/starryeye/auth_service/security/MyAuthenticationSuccessHandler.java
@@ -20,6 +20,7 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
     /**
      * AuthenticationSuccessHandler..
      *      인증에 성공하면 수행할 작업을 담당한다.
+     *      개발자가 직접 커스텀해서 지정해주지 않으면 시큐리티는 기본적으로 SimpleUrlAuthenticationSuccessHandler 를 사용한다.
      *
      * RequestCache..
      *      구현체로 HttpSessionRequestCache 를 가지며..
@@ -37,6 +38,13 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+        /**
+         * 참고로
+         *      로그인 페이지를 요청(로그인 버튼 클릭)해서 로그인을 수행하면 이전 요청은 로그인 페이지가 되는 것이다.
+         *          이때는 savedRequest 가 null 이다.
+         *      회원 전용 페이지를 요청(회원 전용 버튼 클릭)해서 로그인을 수행하면 이전 요청은 회원전용 페이지가 되고..
+         *          이때는 savedRequest 는 null 이 아니며, SavedRequest::getRedirectUrl 은 /user 이다.
+         */
         setDefaultTargetUrl("/");
 
         // RequestCache 로 부터 인증요청 이전의 요청정보(SavedRequest)를 꺼낸다.
