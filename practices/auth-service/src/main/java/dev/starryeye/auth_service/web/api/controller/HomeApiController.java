ac8474d1e7f44992bb0f061eb71443f0ff1284ca
starryeye <see2818@naver.com>
2025-01-01 16:19:23 +0900

    [starryeye, 2025.01.01] - 실습, rest 방식 logout 기능 구현
    내용
    - 실습, rest 방식 logout 기능 구현

--- practices/auth-service/src/main/java/dev/starryeye/auth_service/web/api/controller/HomeApiController.java
+++ practices/auth-service/src/main/java/dev/starryeye/auth_service/web/api/controller/HomeApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api")
 public class HomeApiController {
 
-    @GetMapping()
+    @GetMapping
     public String home() {
         return "/api/home";
     }
--- practices/auth-service/src/main/java/dev/starryeye/auth_service/web/api/controller/LoginApiController.java
+++ practices/auth-service/src/main/java/dev/starryeye/auth_service/web/api/controller/LoginApiController.java
@@ -2,11 +2,13 @@ package dev.starryeye.auth_service.web.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class LoginApiController {
 
-    @GetMapping("/api/login")
+    @GetMapping("/login")
     public String login() {
         /**
          * 해당 페이지에서 비롯된 로그인은 POST /api/login 으로
