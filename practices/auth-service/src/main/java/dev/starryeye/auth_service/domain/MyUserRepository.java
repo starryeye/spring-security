4de4e319e8808d675329f35eb4508fd65ea6d43e
starryeye <see2818@naver.com>
2025-01-04 20:15:28 +0900

    [starryeye, 2025.01.04] - Add Domain Entity
    내용
    - Role, Resource

--- practices/auth-service/src/main/java/dev/starryeye/auth_service/domain/MyUserRepository.java
+++ practices/auth-service/src/main/java/dev/starryeye/auth_service/domain/MyUserRepository.java
@@ -1,10 +1,21 @@
 package dev.starryeye.auth_service.domain;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MyUserRepository extends JpaRepository<MyUser, Long> {
 
-    Optional<MyUser> findByUsername(String username);
+//    @Query("""
+//    select u
+//    from MyUser u
+//        join fetch u.roles ur
+//        join fetch ur.myRole r
+//    where u.username = :username
+//    """)
+    @EntityGraph(attributePaths = {"roles", "roles.myRole"})
+    Optional<MyUser> findByUsername(@Param("username") String username);
 }
--- practices/auth-service/src/main/resources/sql/data.sql
+++ practices/auth-service/src/main/resources/sql/data.sql
@@ -2,7 +2,8 @@ INSERT INTO USERS (username, password, age, created_at, updated_at)
 VALUES ('admin', '{bcrypt}$2a$10$zaIqU9QHGOGL.Jv2tfO/8eSmHpdEa469tRoPcGYOCEGIzEtvPbMY2', 0, NOW(), NOW());
 
 INSERT INTO ROLE (name, description, is_expression, created_at, updated_at)
-VALUES ('ROLE_ADMIN', '관리자', false, NOW(), NOW());
+VALUES ('ROLE_ADMIN', '관리자', false, NOW(), NOW()),
+       ('ROLE_USER', '회원', false, NOW(), NOW());
 
 INSERT INTO USER_ROLE (user_id, role_id, created_at, updated_at)
 VALUES (
