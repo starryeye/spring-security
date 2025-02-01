e4ec2826547d7307de146a108e1a2be9f1833df1
starryeye <see2818@naver.com>
2025-02-01 20:39:37 +0900

    [starryeye, 2025.02.01] - Custom SecurityConfigurer
    내용
    - Custom SecurityConfigurer

--- custom-security-configurer/src/main/java/dev/starryeye/custom_security_configurer/MySecurityConfigurer.java
+++ custom-security-configurer/src/main/java/dev/starryeye/custom_security_configurer/MySecurityConfigurer.java
@@ -8,8 +8,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 public class MySecurityConfigurer extends AbstractHttpConfigurer<MySecurityConfigurer, HttpSecurity> {
 
     /**
-     * HttpSecurity 에 SecurityConfigurer 를 적용시킨다. (SecurityConfig 참고)
-     * HttpSecurity 가 build 메서드를 호출하면, SecurityConfigurer 클래스의 init, configure 메소드를 호출하게되고
+     * HttpSecurity(SecurityBuilder) 에 SecurityConfigurer 를 적용시킨다. (SecurityConfig 참고)
+     * HttpSecurity(SecurityBuilder) 가 build 메서드를 호출하면, SecurityConfigurer 클래스의 init, configure 메소드를 호출하게되고
      * SecurityConfigurer 는 filter 및 해당 filter 의 역할에 필요한 객체들을 초기화 한다.
      * 만들어진 filter 는 최종적으로 SecurityFilterChain 가 관리하는 filter 중 일부가 된다.
      */
