b303810bd0205d514c033d039b02e4df95a11174
starryeye <33487061+starryeye@users.noreply.github.com>
2025-02-07 01:04:46 +0900

    Update CustomAuthenticationEntryPoint.java

--- custom-authenticate-http-basic/src/main/java/dev/starryeye/custom_authenticate_http_basic/CustomAuthenticationEntryPoint.java
+++ custom-authenticate-http-basic/src/main/java/dev/starryeye/custom_authenticate_http_basic/CustomAuthenticationEntryPoint.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.setHeader("WWW-Authenticate", "Basic realm=\"realm\"");
+        response.setHeader("WWW-Authenticate", "Basic realm=\"realm\""); // WWW-Authenticate 가 아닌 다른 문자열을 사용하면 웹 브라우저가 인지 하지 못하여 아이디/비밀번호 입력 창을 띄우지 않는다.
         response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
     }
 }
