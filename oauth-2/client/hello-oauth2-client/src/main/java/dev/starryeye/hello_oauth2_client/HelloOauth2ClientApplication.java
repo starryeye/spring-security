141c2d453c5c18011bd666f874f478ec4b87bce1
starryeye <see2818@naver.com>
2025-02-16 17:23:02 +0900

    [starryeye, 2025.02.16] - OAuth 2.0 Client, spring-oauth2-client
    내용
    - OAuth 2.0 Client, spring-oauth2-client

---
+++ oauth-2/client/hello-oauth2-client/src/main/java/dev/starryeye/hello_oauth2_client/HelloOauth2ClientApplication.java
@@ -0,0 +1,26 @@
+package dev.starryeye.hello_oauth2_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloOauth2ClientApplication {
+
+	/**
+	 * OAuth 2.0 Client 모듈을 구현하려면..
+	 * implementation 'org.springframework.boot:spring-boot-starter-oauth2-client' 의존성을 추가한다.
+	 *
+	 * oauth2-client 의존성을 추가하면, oauth 2.0 기반으로 동작하기 때문에
+	 * spring security 의존성을 추가했을때 기본적으로 id / password 를 제공하지 않는다.
+	 *
+	 * oauth2-client ..
+	 * - Authorization server 및 Resource server 와의 통신을 담당하는 Client 의 기능을 필터기반으로 구현하였다.
+	 * - Resource owner 를 외부 OAuth 2.0 provider 나 OpenID Connect Provider 계정으로 로그인할 수 있는 기능을 제공한다.
+	 * - Authorization code grant, Client credentials grant, Resource owner password credentials grant, Refresh token grant 와 같은 방식을 지원한다.
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloOauth2ClientApplication.class, args);
+	}
+
+}
---
+++ oauth-2/client/hello-oauth2-client/src/test/java/dev/starryeye/hello_oauth2_client/HelloOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.hello_oauth2_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloOauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
