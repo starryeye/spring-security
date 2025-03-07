229bcaf96c1e3442fef109b9f018d3a59a9aafe0
starryeye <see2818@naver.com>
2025-03-07 22:47:39 +0900

    [starryeye, 2025.03.07] - OAuth 2.0 Client, OAuth2UserService
    내용
    - OAuth 2.0 Client, OAuth2UserService

--- oauth-2/client/custom-oauth2-login-oauth2-user-service/src/main/java/dev/starryeye/custom_oauth2_login_oauth2_user_service/MyOidcUserService.java
+++ oauth-2/client/custom-oauth2-login-oauth2-user-service/src/main/java/dev/starryeye/custom_oauth2_login_oauth2_user_service/MyOidcUserService.java
@@ -24,15 +24,28 @@ public class MyOidcUserService extends OidcUserService {
      *      Filter 가 ProviderManager 로 AuthenticationProvider 에게 토큰 교환 및 인증 처리를 위임하는데.. 두가지로 나뉜다.
      *          1. OAuth 2.0 -> OAuth2LoginAuthenticationProvider 로 위임
      *          2. OpenID Connect 1.0 -> OidcAuthorizationCodeAuthenticationProvider 로 위임
+     *      AuthenticationProvider 의 리턴 값인 OAuth2LoginAuthenticationToken 를 OAuth2AuthenticationToken(인증 객체, 사용자 정보 및 권한) 로 변환
+     *          해당 인증 객체는 OAuth2LoginAuthenticationFilter 의 상위 클래스인 AbstractAuthenticationProcessingFilter::successfulAuthentication 에서 SecurityContextRepository 에 저장한다.
+     *      OAuth2AuthorizedClient (access token, refresh token 등이 존재) 를 생성
+     *          해당 객체는 OAuth2AuthorizedClientRepository 에 저장한다.
+     *
      *
      * OAuth2LoginAuthenticationProvider 는..
      *      code 로 access token 교환을 위해, OAuth2AuthorizationCodeAuthenticationProvider 를 이용한다.
      *      access token 으로 userinfo 요청하여 사용자 인증 처리를 위해, DefaultOAuth2UserService 를 이용한다.
+     *      인증 처리 이후 OAuth2LoginAuthenticationToken(인증 객체, 사용자 정보 및 권한) 를 리턴한다.
      * OidcAuthorizationCodeAuthenticationProvider 는..
      *      code 로 access token 및 id token 교환을 위해, OAuth2AccessTokenResponseClient 를 이용한다.
-     *      id token 을 검증을 함으로써 인증 처리를 한다. 이를 위해 OidcUserService 를 이용한다. (실제 인증은 OidcAuthorizationCodeAuthenticationProvider 에서 이루어지며, JWT 를 검증함으로써 인증 처리된다.)
+     *      id token 을 검증을 함으로써 인증 처리를 한다. 이를 위해 OidcUserService 를 이용한다. (실제 인증은 OidcAuthorizationCodeAuthenticationProvider::createOidcToken(), JwtDecoder 에 의해 이루어지며, JWT 를 검증함으로써 인증 처리된다.)
      *          참고.. OidcUserService 내부에서 DefaultOAuth2UserService 의존성을 가지고 있다.
-     *              OidcUserService::shouldRetrieveUserInfo 결과값에 의해 userinfo 요청을 한다. (userinfo 요청을 해서 채워야할 데이터가 있는지 조건 체크인듯..)
+     *              OidcUserService::shouldRetrieveUserInfo 결과값에 의해 userinfo 요청을 한다. (scope 에 openid 이외에 profile, email, address, phone 4가지 중 하나라도 존재하는지 체크)
+     *      인증 처리 이후 OAuth2LoginAuthenticationToken(인증 객체, 사용자 정보 및 권한) 를 리턴한다.
+     *
+     * 참고
+     * 인가 서버마다 사용자를 식별하는 username 에 해당하는 필드 이름이 다르다. (표준은 sub 이며, google 이 사용한다. keycloak 은 preferred_username 이다.)
+     *      id token 에 기본적으로 사용자 정보들이 존재하는데.. 여기에는 sub 이 기본적으로 존재한다.
+     *          하지만, keycloak 의 preferred_username 은 존재하지 않는다. 그래서 keycloak 은 scope 에 profile 을 추가해서..
+     *          "/userinfo" 요청을 추가로 수행하여, scope.. profile 을 추가했을때 받을 수 있는 preferred_username 이 사용자 정보에 포함되도록 해야한다.
      *
      */
 
