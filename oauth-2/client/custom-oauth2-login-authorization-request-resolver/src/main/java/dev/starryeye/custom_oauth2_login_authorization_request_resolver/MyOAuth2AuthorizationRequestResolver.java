bad163521197a12a8947d81c281ba7b1bdeef842
starryeye <see2818@naver.com>
2025-03-13 06:02:25 +0900

    [starryeye, 2025.03.13] - OAuth 2.0 Client, oauth2Login - authorizationRequestResolver and PKCE
    내용
    - OAuth 2.0 Client, oauth2Login - authorizationRequestResolver and PKCE

---
+++ oauth-2/client/custom-oauth2-login-authorization-request-resolver/src/main/java/dev/starryeye/custom_oauth2_login_authorization_request_resolver/MyOAuth2AuthorizationRequestResolver.java
@@ -0,0 +1,95 @@
+package dev.starryeye.custom_oauth2_login_authorization_request_resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class MyOAuth2AuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver {
+
+    /**
+     * PKCE 가 정상동작 하도록 커스텀하였다.
+     */
+
+    private static final String REGISTRATION_ID_URI_VARIABLE_NAME = "registrationId";
+    private static final Consumer<OAuth2AuthorizationRequest.Builder> DEFAULT_PKCE_APPLIER = OAuth2AuthorizationRequestCustomizers
+            .withPkce();
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final AntPathRequestMatcher authorizationRequestMatcher;
+
+    // 원래 개발자의 커스텀 OAuth2AuthorizationRequestResolver 이 없으면, DefaultOAuth2AuthorizationRequestResolver 객체가 사용된다.
+    private final DefaultOAuth2AuthorizationRequestResolver defaultOAuth2AuthorizationRequestResolver;
+
+    public MyOAuth2AuthorizationRequestResolver(ClientRegistrationRepository clientRegistrationRepository, String  authorizationRequestBaseUri) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+        this.authorizationRequestMatcher = new AntPathRequestMatcher(
+                authorizationRequestBaseUri + "/{" + REGISTRATION_ID_URI_VARIABLE_NAME + "}");
+
+        defaultOAuth2AuthorizationRequestResolver = new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, authorizationRequestBaseUri);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request) {
+
+        String clientRegistrationId = resolveRegistrationId(request);
+        if (clientRegistrationId == null) {
+            return null;
+        }
+
+        if ("keycloak-authorization-code-with-pkce".equals(clientRegistrationId)) { // pkce 요청이면 추가적인 작업을 해준다.
+            OAuth2AuthorizationRequest oAuth2AuthorizationRequest = defaultOAuth2AuthorizationRequestResolver.resolve(request);
+            return customResolve(oAuth2AuthorizationRequest);
+        }
+
+        return defaultOAuth2AuthorizationRequestResolver.resolve(request);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request, String clientRegistrationId) {
+
+        if ("keycloak-authorization-code-with-pkce".equals(clientRegistrationId)) { // pkce 요청이면 추가적인 작업을 해준다.
+            OAuth2AuthorizationRequest oAuth2AuthorizationRequest = defaultOAuth2AuthorizationRequestResolver.resolve(request); // pkce 용 OAuth2AuthorizationRequest 가 아니라, 기본 OAuth2AuthorizationRequest 이 리턴된다.
+            return customResolve(oAuth2AuthorizationRequest);
+        }
+
+        return defaultOAuth2AuthorizationRequestResolver.resolve(request, clientRegistrationId);
+    }
+
+    private OAuth2AuthorizationRequest customResolve(OAuth2AuthorizationRequest oAuth2AuthorizationRequest) {
+
+        /**
+         *  application.yml 에 pkce 설정에 보면, client-authentication-method 이 "none" 이 아니기 때문에
+         *  PKCE 용 request 가 만들어지지 않는다. 그래서 DEFAULT_PKCE_APPLIER.accept(builder) 를 통해 PKCE 용 request 로 만들어준다.
+         *
+         *  참고, 아래와 같이 표준 파라미터가 아닌 커스텀 파라미터를 넣어 줄 수 있다. (extra)
+         */
+
+        Map<String, Object> extra = Map.of(
+                "customKey1", "customValue1",
+                "customKey2", "customValue2"
+        );
+
+        OAuth2AuthorizationRequest.Builder builder = OAuth2AuthorizationRequest.from(oAuth2AuthorizationRequest)
+                .additionalParameters(extra)
+                ;
+        DEFAULT_PKCE_APPLIER.accept(builder); // PKCE 용으로 만들어 줌, code_challenge, code_challenge_method 값 생성
+
+        return builder.build();
+    }
+
+    private String resolveRegistrationId(HttpServletRequest request) {
+        if (this.authorizationRequestMatcher.matches(request)) {
+            return this.authorizationRequestMatcher.matcher(request)
+                    .getVariables()
+                    .get(REGISTRATION_ID_URI_VARIABLE_NAME);
+        }
+        return null;
+    }
+}
--- oauth-2/client/custom-oauth2-login-authorization-request-resolver/src/main/java/dev/starryeye/custom_oauth2_login_authorization_request_resolver/OAuth2ClientConfig.java
+++ oauth-2/client/custom-oauth2-login-authorization-request-resolver/src/main/java/dev/starryeye/custom_oauth2_login_authorization_request_resolver/OAuth2ClientConfig.java
@@ -3,10 +3,10 @@ package dev.starryeye.custom_oauth2_login_authorization_request_resolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -17,19 +17,25 @@ public class OAuth2ClientConfig {
     private final ClientRegistrationRepository clientRegistrationRepository;
 
     /**
-     * Authorization code grant with PKCE 를 Client 에 적용시키고 싶다면..
+     * Authorization code grant with PKCE 를 Client 에 적용시키고 싶은데..
+     *      authorization code 로 access token, id token 을 얻는 요청(2 단계 요청) 에서 client_secret 데이터를 함께 넣어줘야할때는..?
      *
      * OAuth2AuthorizationRequestResolver 를 자세히 알아야한다. (구현체 : DefaultOAuth2AuthorizationRequestResolver)
-     *      Authorization code 를 authorization server 에 요청 하기 위해 만든 OAuth2AuthorizationRequest 를 생성한다.
-     *      만들때..
-     *          ClientAuthenticationMethod.NONE.equals(clientRegistration.getClientAuthenticationMethod()) 조건을 보고 PKCE 용으로 OAuth2AuthorizationRequest 만들게 된다.
+     *      Authorization code 를 authorization server 에 요청(1 단계 요청) 하기 위한 OAuth2AuthorizationRequest 를 생성한다.
+     *      생성할때..
+     *          ClientAuthenticationMethod.NONE.equals(clientRegistration.getClientAuthenticationMethod()) 조건을 보고 PKCE 용으로 OAuth2AuthorizationRequest 를 생성하게된다.
      *              따라서, application.yml 에 "client-authentication-method: none" 설정이 필요하다.
      *              OAuth2AuthorizationRequestCustomizers::applyPkce 로 설정 및 생성
      *                  code_challenge, code_challenge_method 값 생성
      *                      spring oauth2 client 는 기본적으로 S256 로 한다. (-> keycloak client 설정에서 advanced > Proof Key for Code Exchange Code Challenge Method 를 S256 으로 설정 필요)
      *              그런데, "client-authentication-method: none" 설정을 하면..
-     *                  authorization code 로 access token, id token 을 얻는 요청을 할때..
-     *                      client_secret 을 요청데이터에 넣지 않아 keycloak 에서 에러 리턴한다..
+     *                  2 단계 요청을 할때.. client_secret 을 요청데이터에 넣지 않아 keycloak 에서 에러 리턴한다..
+     *
+     *              그래서, MyOAuth2AuthorizationRequestResolver 로 해결해보겠다.
+     *                  "client-authentication-method: none" 이 아니라, "client-authentication-method: client_secret_basic" 으로 기본적인 authorization code grant 방식과 동일하게 셋팅하고..
+     *                      registration id 를 바탕으로 분기하여 OAuth2AuthorizationRequest 를 생성하도록 한다.
+     *                          pkce 용 registration id 로 요청이 오면 pkce 용 OAuth2AuthorizationRequest 를 생성하도록한다.
+     *                      "client-authentication-method: client_secret_basic" 로 설정하였기 때문에 2 단계 요청에서 client_secret 을 요청데이터에 넣는다.
      *
      */
 
@@ -42,7 +48,13 @@ public class OAuth2ClientConfig {
                                 .requestMatchers("/").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oAuth2LoginConfigurer ->
+                        oAuth2LoginConfigurer
+                                .authorizationEndpoint(authorizationEndpointConfigurer ->
+                                        authorizationEndpointConfigurer
+                                                .authorizationRequestResolver(myOAuth2AuthorizationRequestResolver())
+                                )
+                )
                 .logout(httpSecurityLogoutConfigurer ->
                                 httpSecurityLogoutConfigurer
                                         .logoutSuccessHandler(oidcLogoutSuccessHandler()) // front channel logout 기능
@@ -53,6 +65,10 @@ public class OAuth2ClientConfig {
                 ;
 
         return http.build();
+    }
+
+    private OAuth2AuthorizationRequestResolver myOAuth2AuthorizationRequestResolver() {
+        return new MyOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
 
     private LogoutSuccessHandler oidcLogoutSuccessHandler() {
