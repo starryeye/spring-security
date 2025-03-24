974549a7ac78b23ba3067b9cd00e1eb546609e88
starryeye <see2818@naver.com>
2025-03-24 21:14:12 +0900

    [starryeye, 2025.03.24] - OAuth 2.0, social login client
    내용
    - OAuth 2.0, social login client

---
+++ oauth-2/client/custom-social-login-client/src/main/java/dev/starryeye/custom_social_login_client/CustomSocialLoginClientApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_social_login_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSocialLoginClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSocialLoginClientApplication.class, args);
+	}
+
+}
---
+++ oauth-2/client/custom-social-login-client/src/test/java/dev/starryeye/custom_social_login_client/CustomSocialLoginClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_social_login_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSocialLoginClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
