28aaf7b23091a032720aa2aefdfd98193093cc8a
starryeye <see2818@naver.com>
2025-03-26 20:38:49 +0900

    [starryeye, 2025.03.26] - OAuth 2.0, social login client
    내용
    - OAuth 2.0, social login client

---
+++ oauth-2/client/custom-social-login-client/src/main/java/dev/starryeye/custom_social_login_client/controller/HomeController.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_social_login_client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
---
+++ oauth-2/client/custom-social-login-client/src/main/java/dev/starryeye/custom_social_login_client/controller/IndexController.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_social_login_client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
