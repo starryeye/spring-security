d4f08f4b0084dfee0fcfdfe42ffb96f6e2b65da6
starryeye <see2818@naver.com>
2025-03-30 20:46:21 +0900

    [starryeye, 2025.03.30] - OAuth 2.0, custom social login client with form login
    내용
    - OAuth 2.0, custom social login client with form login

---
+++ oauth-2/client/custom-social-login-client-with-form-login/src/main/java/dev/starryeye/custom_social_login_client_with_form_login/CustomSocialLoginClientWithFormLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_social_login_client_with_form_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSocialLoginClientWithFormLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSocialLoginClientWithFormLoginApplication.class, args);
+	}
+
+}
---
+++ oauth-2/client/custom-social-login-client-with-form-login/src/test/java/dev/starryeye/custom_social_login_client_with_form_login/CustomSocialLoginClientWithFormLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_social_login_client_with_form_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSocialLoginClientWithFormLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
