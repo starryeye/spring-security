cad55dd6866d99df02a09477e43510e781bd10b3
starryeye <see2818@naver.com>
2025-04-11 03:25:01 +0900

    [starryeye, 2025.04.11] - OAuth 2.0, Hello oauth2-resource-server
    내용
    - OAuth 2.0, oauth2-resource-server

--- oauth-2/resource-server/hello-oauth2-resource-server/src/main/java/dev/starryeye/hello_oauth2_resource_server/HelloController.java
+++ oauth-2/resource-server/hello-oauth2-resource-server/src/main/java/dev/starryeye/hello_oauth2_resource_server/HelloController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     /**
-     * 현재, "/" 에 access 하기 위해서는 인증이 필요하다.
+     * 현재, "/" 에 access 하기 위해서는 인증이 필요하다.(기본)
      * 해당 프로젝트는 resource-server 로써 authorization server 로 통신이 가능하다.
      *      해당 "/" 요청의 권한을 승인하는 절차를 authorization server 로 위임할 수 있음.
      *      요청 시, Authorization header 에 "bearer {access token}" 값을 함께 넣어주면.. (api.http 참고)
      *          authorization server 를 통해 해당 access token 에 대하여 검증을 수행하고
      *          최종 인증 처리를 한다.
+     *
+     * 참고.
+     * 뭐에 대해서 무슨 인증 처리를 했는지는.. 요청 응답인 Authentication 을 참고해보자..
      */
 
     @GetMapping("/")
