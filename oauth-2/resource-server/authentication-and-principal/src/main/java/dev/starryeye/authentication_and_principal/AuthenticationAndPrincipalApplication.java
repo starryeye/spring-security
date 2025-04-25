829a24daf25d957d1fe53f41d1609adb10eb121f
starryeye <see2818@naver.com>
2025-04-25 23:11:19 +0900

    [starryeye, 2025.04.25] - OAuth 2.0, oauth2-resource-server Authentication, @AuthenticationPrincipal
    내용
    - Authentication, @AuthenticationPrincipal, ArgumentResolver

---
+++ oauth-2/resource-server/authentication-and-principal/src/main/java/dev/starryeye/authentication_and_principal/AuthenticationAndPrincipalApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.authentication_and_principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAndPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAndPrincipalApplication.class, args);
+	}
+
+}
---
+++ oauth-2/resource-server/authentication-and-principal/src/test/java/dev/starryeye/authentication_and_principal/AuthenticationAndPrincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.authentication_and_principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndPrincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
