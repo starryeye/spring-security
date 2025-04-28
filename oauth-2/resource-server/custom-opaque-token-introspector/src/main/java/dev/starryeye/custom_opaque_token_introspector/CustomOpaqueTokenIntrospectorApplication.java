f5b5341282a1452fc1599a22be8427a8bbe69417
starryeye <see2818@naver.com>
2025-04-29 02:20:13 +0900

    [starryeye, 2025.04.29] - OAuth 2.0, oauth2-resource-server OpaqueTokenIntrospector
    내용
    - OpaqueTokenIntrospector

---
+++ oauth-2/resource-server/custom-opaque-token-introspector/src/main/java/dev/starryeye/custom_opaque_token_introspector/CustomOpaqueTokenIntrospectorApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_opaque_token_introspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomOpaqueTokenIntrospectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomOpaqueTokenIntrospectorApplication.class, args);
+	}
+
+}
---
+++ oauth-2/resource-server/custom-opaque-token-introspector/src/test/java/dev/starryeye/custom_opaque_token_introspector/CustomOpaqueTokenIntrospectorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_opaque_token_introspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomOpaqueTokenIntrospectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
