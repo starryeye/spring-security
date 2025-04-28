55a27077f17ba60e696cd4dc3c757c51ced99bb8
starryeye <see2818@naver.com>
2025-04-28 20:16:07 +0900

    [starryeye, 2025.04.28] - OAuth 2.0, oauth2-resource-server JwtAuthenticationConverter
    내용
    - JwtAuthenticationConverter

---
+++ oauth-2/resource-server/custom-jwt-authentication-converter/src/main/java/dev/starryeye/custom_jwt_authentication_converter/CustomJwtAuthenticationConverterApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_jwt_authentication_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomJwtAuthenticationConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomJwtAuthenticationConverterApplication.class, args);
+	}
+
+}
---
+++ oauth-2/resource-server/custom-jwt-authentication-converter/src/test/java/dev/starryeye/custom_jwt_authentication_converter/CustomJwtAuthenticationConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_jwt_authentication_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomJwtAuthenticationConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
