62231fa991ea6fcdee2c032ba63d4861cf9db508
starryeye <see2818@naver.com>
2025-05-04 20:32:37 +0900

    [starryeye, 2025.05.04] - OAuth 2.0, simple-integration-client-and-resource-server init
    내용
    - OAuth 2.0, simple-integration-client-and-resource-server init

---
+++ oauth-2/practices/simple-integration-client-and-resource-server/oauth2-client/src/test/java/dev/starryeye/oauth2_client/Oauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.oauth2_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
