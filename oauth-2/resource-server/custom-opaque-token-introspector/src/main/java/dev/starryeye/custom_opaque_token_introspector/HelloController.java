7c1f5ed20869ba47d613d08d5c856106d16f0b5d
starryeye <see2818@naver.com>
2025-05-04 18:42:57 +0900

    [starryeye, 2025.05.04] - OAuth 2.0, oauth2-resource-server OpaqueTokenIntrospector
    내용
    - OpaqueTokenIntrospector

--- oauth-2/resource-server/custom-opaque-token-introspector/src/main/java/dev/starryeye/custom_opaque_token_introspector/HelloController.java
+++ oauth-2/resource-server/custom-opaque-token-introspector/src/main/java/dev/starryeye/custom_opaque_token_introspector/HelloController.java
@@ -1,14 +1,33 @@
 package dev.starryeye.custom_opaque_token_introspector;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
+import org.springframework.security.oauth2.server.resource.introspection.OAuth2IntrospectionAuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public Authentication hello(Authentication authentication) {
         return authentication;
+    }
+
+    @GetMapping("/authentication-principal")
+    public Authentication authenticationPrincipal(Authentication authentication, @AuthenticationPrincipal OAuth2IntrospectionAuthenticatedPrincipal principal) {
+
+        // opaqueToken() 설정에서는 최종 인증객체 타입이 BearerTokenAuthentication 이다.
+        BearerTokenAuthentication bearerTokenAuthentication = (BearerTokenAuthentication) authentication;
+        log.info("token active = {}", bearerTokenAuthentication.getTokenAttributes().get("active"));
+
+        // opaqueToken() 설정에서는 인증객체의 principal 에 저장 되는 타입은 OAuth2IntrospectionAuthenticatedPrincipal 이다.
+        log.info("principal = {}", principal);
+
+        return bearerTokenAuthentication;
     }
 }
--- oauth-2/resource-server/custom-opaque-token-introspector/src/main/java/dev/starryeye/custom_opaque_token_introspector/OAuth2ResourceServerConfig.java
+++ oauth-2/resource-server/custom-opaque-token-introspector/src/main/java/dev/starryeye/custom_opaque_token_introspector/OAuth2ResourceServerConfig.java
@@ -19,6 +19,20 @@ public class OAuth2ResourceServerConfig {
      * authorization server 에 introspect endpoint 요청을 하여 토큰 검증을 위임한다.
      */
 
+    /**
+     * 기본적인 검증 흐름.
+     * BearerTokenAuthenticationFilter (jwt 설정과 동일)
+     *      미 인증 객체 생성 : BearerTokenAuthenticationToken (jwt 설정과 동일)
+     *      AuthenticationManager(ProviderManager) 를 거쳐서 OpaqueTokenAuthenticationProvider 에 인증 처리 위임
+     *      인증 처리 후, SecurityContext 에 인증 객체 저장
+     * OpaqueTokenAuthenticationProvider (jwt 설정으로 치면, JwtAuthenticationProvider)
+     *      OpaqueTokenIntrospector 에 토큰 검증 수행 위임 (jwt 설정으로 치면, JwtDecoder)
+     *          검증 후 OAuth2IntrospectionAuthenticatedPrincipal 리턴됨. (jwt 설정으로 치면, Jwt)
+     *      인증 객체 생성 및 리턴 : BearerTokenAuthentication (jwt 설정으로 치면, JwtAuthenticationToken)
+     *          principal : OAuth2IntrospectionAuthenticatedPrincipal (jwt 설정으로 치면, Jwt)
+     *
+     */
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -41,7 +55,7 @@ public class OAuth2ResourceServerConfig {
      *      OAuth2ResourceServerOpaqueTokenConfiguration::opaqueTokenIntrospector() 에서..
      *          OpaqueTokenIntrospector 빈을 자동 구성 등록하고 있다.
      *      jwt 설정으로 치면, JwtDecoder 의 역할과 동일하다. (토큰 검증)
-     *      아래와 같이 직접 등록하면 등록한 빈이 사용된다.
+     *      아래와 같이 직접 등록하면 등록한 빈이 사용된다. (혹은 oauth2ResourceServer().opaqueToken() 의 introspector 로 직접 설정가능)
      *
      * 참고.
      * resource server 에서 client id/password 를 설정해야하는데.. (application.yml 참고)
