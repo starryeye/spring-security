4729696c7ac7abfe449006ed81c437c7d48f4c3d
starryeye <see2818@naver.com>
2025-05-05 21:06:46 +0900

    [starryeye, 2025.05.05] - OAuth 2.0, Authorization server
    내용
    - OAuth 2.0, Authorization server

---
+++ oauth-2/authorization-server/hello-oauth2-authorization-server/src/main/java/dev/starryeye/hello_oauth2_authorization_server/HelloOauth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.hello_oauth2_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloOauth2AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloOauth2AuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/hello-oauth2-authorization-server/src/test/java/dev/starryeye/hello_oauth2_authorization_server/HelloOauth2AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.hello_oauth2_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloOauth2AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
