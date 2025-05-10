7dc7ba7ceb3120694a517596e13d39f6c3b6a61a
starryeye <see2818@naver.com>
2025-05-10 20:53:59 +0900

    [starryeye, 2025.05.10] - OAuth 2.0 Authorization server, RegisteredClient, RegisteredClientRepository
    내용
    - RegisteredClient, RegisteredClientRepository

---
+++ oauth-2/authorization-server/registered-client-repository/src/main/java/dev/starryeye/registered_client_repository/RegisteredClientRepositoryApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.registered_client_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisteredClientRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisteredClientRepositoryApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/registered-client-repository/src/test/java/dev/starryeye/registered_client_repository/RegisteredClientRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.registered_client_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisteredClientRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
