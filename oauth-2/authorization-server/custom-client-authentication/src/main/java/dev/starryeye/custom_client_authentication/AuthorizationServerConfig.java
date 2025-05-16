b20e4e2b34c69fb3a0d384c1f317ea65d9b173f3
starryeye <see2818@naver.com>
2025-05-16 23:38:37 +0900

    [starryeye, 2025.05.16] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::clientAuthentication
    내용
    - OAuth2AuthorizationServerConfigurer::clientAuthentication

--- oauth-2/authorization-server/custom-client-authentication/src/main/java/dev/starryeye/custom_client_authentication/AuthorizationServerConfig.java
+++ oauth-2/authorization-server/custom-client-authentication/src/main/java/dev/starryeye/custom_client_authentication/AuthorizationServerConfig.java
@@ -36,14 +36,18 @@ import java.util.UUID;
 public class AuthorizationServerConfig {
 
     /**
-     * OAuth2AuthorizationServerConfigurer::clientAuthentication 를 이용하여
+     * OAuth2AuthorizationServerConfigurer::clientAuthentication 를 이용하여 client 인증 처리를 커스텀 할 수 있다.
+     *
+     * client 인증..
      * authorization server 에 client 가 access token 에 관한 요청(발급, 검증, 해지)하면,
-     * 가장 먼저 수행하는 것이 client 인증이다. -> token 관련 요청 필터는 SecurityFilterChain 에서 뒷 순서에 속한다. (필터 중.. AuthorizationFilter 가 token 관련 요청 필터들 앞에 위치하며 client 인증을 받았는지를 체크하는듯)
-     *      POST "/oauth2/token"
-     *      POST "/oauth2/introspect"
-     *      POST "/oauth2/revoke"
-     *      POST "/oauth2/device_authorization"
-     *      client_secret_basic, client_secret_post, private_key_jwt, client_secret_jwt, none 방식
+     * 가장 먼저 수행하는 것이 client 인증이다.
+     *      제공되는 client 인증 방식
+     *          client_secret_basic, client_secret_post, private_key_jwt, client_secret_jwt, none 방식
+     *      관련 path
+     *          POST "/oauth2/token"
+     *          POST "/oauth2/introspect"
+     *          POST "/oauth2/revoke"
+     *          POST "/oauth2/device_authorization"
      *
      * 관련 객체 및 flow
      * OAuth2ClientAuthenticationConfigurer
@@ -63,6 +67,10 @@ public class AuthorizationServerConfig {
      *          X509ClientCertificateAuthenticationProvider
      *      authenticationSuccessHandler
      *      authenticationFailureHandler
+     *
+     * 참고
+     * 실제 access token 관련 요청 필터는 SecurityFilterChain 에서 뒷 순서에 속한다.
+     * 필터 중.. AuthorizationFilter 가 token 관련 요청 필터들 앞에 위치하며 client 인증을 받았는지를 체크하는듯
      */
 
     @Bean
