1a24cbeaf1fc324d7828b6760570a1d062b1b3c3
starryeye <see2818@naver.com>
2025-05-22 23:45:56 +0900

    [starryeye, 2025.05.22] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::tokenIntrospectionEndPoint
    내용
    - OAuth2AuthorizationServerConfigurer::tokenIntrospectionEndPoint

---
+++ oauth-2/authorization-server/custom-token-introspection-endpoint/src/main/java/dev/practice/custom_token_introspection_endpoint/CustomTokenIntrospectionEndpointApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.custom_token_introspection_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomTokenIntrospectionEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomTokenIntrospectionEndpointApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/custom-token-introspection-endpoint/src/test/java/dev/practice/custom_token_introspection_endpoint/CustomTokenIntrospectionEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.custom_token_introspection_endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomTokenIntrospectionEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
