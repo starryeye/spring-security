4c109f9728591ddefb407fe015db702a147e343f
starryeye <see2818@naver.com>
2025-05-24 18:29:16 +0900

    [starryeye, 2025.05.24] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::tokenRevocationEndpoint
    내용
    - OAuth2AuthorizationServerConfigurer::tokenRevocationEndpoint

---
+++ oauth-2/authorization-server/custom-token-revocation-endpoint/src/main/java/dev/starryeye/custom_token_revocation_endpoint/CustomTokenRevocationEndpointApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_token_revocation_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomTokenRevocationEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomTokenRevocationEndpointApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/custom-token-revocation-endpoint/src/test/java/dev/starryeye/custom_token_revocation_endpoint/CustomTokenRevocationEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_token_revocation_endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomTokenRevocationEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
