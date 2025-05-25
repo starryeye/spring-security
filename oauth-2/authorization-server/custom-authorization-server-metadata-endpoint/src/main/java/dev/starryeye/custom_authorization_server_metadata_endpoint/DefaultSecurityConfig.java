e44e7d8d51df4f14bf4355d745265ca0601bd614
starryeye <see2818@naver.com>
2025-05-25 19:54:32 +0900

    [starryeye, 2025.05.25] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::authorizationServerMetadataEndpoint
    내용
    - OAuth2AuthorizationServerConfigurer::authorizationServerMetadataEndpoint

---
+++ oauth-2/authorization-server/custom-authorization-server-metadata-endpoint/src/main/java/dev/starryeye/custom_authorization_server_metadata_endpoint/DefaultSecurityConfig.java
@@ -0,0 +1,43 @@
+package dev.starryeye.custom_authorization_server_metadata_endpoint;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DefaultSecurityConfig {
+
+    /**
+     * OAuth 2.0 Authorization server endpoint 와 관계없는 요청들에 대한 처리를 담당하는 Security 설정
+     */
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .authorities("ROLE_USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
