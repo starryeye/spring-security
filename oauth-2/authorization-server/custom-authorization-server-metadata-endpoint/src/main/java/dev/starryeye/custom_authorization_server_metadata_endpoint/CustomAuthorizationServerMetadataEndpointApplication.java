7cad05a996e046277a290908ef4013c82c7a854e
starryeye <see2818@naver.com>
2025-05-25 17:21:03 +0900

    [starryeye, 2025.05.25] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::authorizationServerMetadataEndpoint
    내용
    - OAuth2AuthorizationServerConfigurer::authorizationServerMetadataEndpoint

---
+++ oauth-2/authorization-server/custom-authorization-server-metadata-endpoint/src/main/java/dev/starryeye/custom_authorization_server_metadata_endpoint/CustomAuthorizationServerMetadataEndpointApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_authorization_server_metadata_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthorizationServerMetadataEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAuthorizationServerMetadataEndpointApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/custom-authorization-server-metadata-endpoint/src/test/java/dev/starryeye/custom_authorization_server_metadata_endpoint/CustomAuthorizationServerMetadataEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_authorization_server_metadata_endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAuthorizationServerMetadataEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
