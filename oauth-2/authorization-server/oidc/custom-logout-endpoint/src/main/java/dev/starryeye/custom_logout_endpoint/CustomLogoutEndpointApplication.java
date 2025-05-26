582d4d9af33053aa22c8813946a08d72f174de3e
starryeye <see2818@naver.com>
2025-05-26 22:22:18 +0900

    [starryeye, 2025.05.26] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::oidc, logoutEndpoint
    내용
    - OAuth2AuthorizationServerConfigurer::oidc, logoutEndpoint

---
+++ oauth-2/authorization-server/oidc/custom-logout-endpoint/src/main/java/dev/starryeye/custom_logout_endpoint/CustomLogoutEndpointApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_logout_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLogoutEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLogoutEndpointApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/oidc/custom-logout-endpoint/src/test/java/dev/starryeye/custom_logout_endpoint/CustomLogoutEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_logout_endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLogoutEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
