8db45663a5eab52a774d44ffbe629dca529517a2
starryeye <see2818@naver.com>
2025-05-26 22:10:44 +0900

    [starryeye, 2025.05.26] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::oidc, providerConfigurationEndpoint
    내용
    - OAuth2AuthorizationServerConfigurer::oidc, providerConfigurationEndpoint

---
+++ oauth-2/authorization-server/oidc/custom-provider-configuration-endpoint/src/main/java/dev/starryeye/custom_provider_configuration_endpoint/CustomProviderConfigurationEndpointApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_provider_configuration_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomProviderConfigurationEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomProviderConfigurationEndpointApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/oidc/custom-provider-configuration-endpoint/src/test/java/dev/starryeye/custom_provider_configuration_endpoint/CustomProviderConfigurationEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_provider_configuration_endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomProviderConfigurationEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
