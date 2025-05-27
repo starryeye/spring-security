1036982fbfdda96962236f78f1e75dfb51cff968
starryeye <see2818@naver.com>
2025-05-27 21:22:59 +0900

    [starryeye, 2025.05.27] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::oidc, logoutEndpoint
    내용
    - OAuth2AuthorizationServerConfigurer::oidc, logoutEndpoint

---
+++ oauth-2/authorization-server/oidc/custom-logout-endpoint/src/main/java/dev/starryeye/custom_logout_endpoint/AuthorizationServerConfig.java
@@ -0,0 +1,205 @@
+package dev.starryeye.custom_logout_endpoint;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+    /**
+     * OAuth2AuthorizationServerConfigurer::oidc, providerConfigurationEndpoint() api 를 이용하여
+     * OpenID Connect 1.0 Provider Configuration endpoint 를 커스텀해본다.
+     *      GET "/.well-known/openid-configuration" (OIDC 관련 엔드포인트 메타데이터 정보를 응답하는 엔드포인트이다.)
+     *
+     * 주요 클래스
+     * OidcProviderConfigurationEndpointConfigurer
+     * OidcProviderConfigurationEndpointFilter
+     *      AuthorizationServerSettings 를 참조하여 OidcProviderConfiguration 를 생성 후 응답
+     * 	    	OidcProviderConfiguration 생성 시, 개발자가 OAuth2AuthorizationServerConfigurer::authorizationServerMetadataEndpoint() 를 통해 설정한 값으로 덮어쓰기도 함.
+     *
+     * 주의 사항..
+     * OAuth2AuthorizationServerMetadataEndpointFilter 에서..
+     * OAuth2AuthorizationServerMetadata 생성할 때, RegisteredClientRepository 를 참조하지는 않기 때문에..
+     * 현재 제공되는 기능과 1:1 매칭이 안되는듯..
+     *
+     */
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+        /**
+         * 아래 주석.. OAuth2AuthorizationServerConfigurer::oidc(), providerConfigurationEndpoint()..
+         *
+         * OIDC 관련 메타데이터를 볼 수 있는 요청("/.well-known/openid-configuration") endpoint 에 대한 설정을
+         * 커스텀하게 할 수 있도록 제공하는 api 이다.
+         */
+
+        http
+                .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher()) // authorization server 에서 제공하는 기본 EndPoint 에 대한 SecurityFilterChain
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .anyRequest().authenticated()
+                )
+                .with(authorizationServerConfigurer, oAuth2AuthorizationServerConfigurer ->
+                        oAuth2AuthorizationServerConfigurer
+                                // 기본적으로 oidc 는 disable 되어있어서 설정해줘야함.
+                                .oidc(Customizer.withDefaults())
+//                                .oidc(oidcConfigurer ->
+//                                        oidcConfigurer
+//                                                .providerConfigurationEndpoint(oidcProviderConfigurationEndpointConfigurer ->
+//                                                        oidcProviderConfigurationEndpointConfigurer
+//                                                                .providerConfigurationCustomizer(oidcProviderConfiguration ->
+//                                                                        oidcProviderConfiguration
+//                                                                                .endSessionEndpoint("")
+//                                                                                .authorizationEndpoint("")
+//                                                                                .idTokenSigningAlgorithm("")
+//                                                                                .subjectType("")
+//                                                                                .userInfoEndpoint("")
+//                                                                                .build()
+//                                                                )
+//                                                )
+//                                )
+                )
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer
+                                // 기본 authentication EntryPoint 설정
+                                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                )
+        ;
+
+        return http.build();
+    }
+
+    // application.yml 설정을 이용하여 자동 구성되도록 하지 않고.. 직접 등록함
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder()
+                .issuer("http://localhost:8091")
+                .build();
+    }
+
+    // application.yml 설정을 이용하여 자동 구성되도록 하지 않고.. 직접 등록함
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+
+        // confidential client 용 (PKCE, authorization code, client credential, refresh token 지원)
+        RegisteredClient registeredClient1 = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("my-spring-client")
+                .clientSecret("{noop}secret")
+                .clientIdIssuedAt(Instant.now())
+                .clientSecretExpiresAt(Instant.MAX)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/my-spring-client")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("custom-scope")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build()) // authorization code 요청시 resource owner 에게 consent 동의 화면 보여줄 것인지 설정
+                .tokenSettings(TokenSettings.builder().reuseRefreshTokens(false).build()) // refresh token grant 시, refresh token 을 재사용할 것인지 설정 (기본 값 true)
+                .build();
+
+        // public client 용 (PKCE 지원)
+        RegisteredClient registeredClient2 = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("my-public-client")
+                .clientIdIssuedAt(Instant.now())
+                .clientSecretExpiresAt(Instant.MAX)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE) // public client 용으로 client id + PKCE 로 client 인증을 위함 (client secret 사용하지않음)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN) // 주의, OAuth2RefreshTokenGenerator 에서 refresh token 을 생성할 때, public client 라면 refresh token 을 생성하지 않는 것으로 되어 있다.
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/my-public-client")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("custom-scope")
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true) // authorization code 요청시 resource owner 에게 consent 동의 화면 보여줄 것인지 설정
+                        .requireProofKey(true) // PKCE 필수화, 요청에 code_challenge, code_challenge_method 를 반드시 포함시켜야함.
+                        .build()
+                )
+                .tokenSettings(TokenSettings.builder().reuseRefreshTokens(false).build()) // refresh token grant 시, refresh token 을 재사용할 것인지 설정 (기본 값 true)
+                .build();
+
+        // resource server 용
+        RegisteredClient registeredClient3 = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("my-spring-resource-server")
+                .clientSecret("{noop}secret-resource-server")
+                .clientIdIssuedAt(Instant.now())
+                .clientSecretExpiresAt(Instant.MAX)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/my-spring-resource-server")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("custom-scope")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build()) // authorization code 요청시 resource owner 에게 consent 동의 화면 보여줄 것인지 설정
+                .build();
+
+        /**
+         * ClientSettings.builder(), TokenSettings.builder() 에도 다양한 설정이 존재한다.
+         *      위 설정 외에도.. time to live 등..
+         */
+
+        return new InMemoryRegisteredClientRepository(List.of(registeredClient1, registeredClient2, registeredClient3));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        RSAKey rsaKey = generateRsa();
+        return new ImmutableJWKSet<>(new JWKSet(rsaKey));
+    }
+
+    private RSAKey generateRsa() {
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            KeyPair keyPair = generator.generateKeyPair();
+            return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                    .privateKey((RSAPrivateKey) keyPair.getPrivate())
+                    .keyID(UUID.randomUUID().toString())
+                    .build();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
--- oauth-2/authorization-server/oidc/custom-logout-endpoint/src/main/java/dev/starryeye/custom_logout_endpoint/CustomLogoutEndpointApplication.java
+++ oauth-2/authorization-server/oidc/custom-logout-endpoint/src/main/java/dev/starryeye/custom_logout_endpoint/CustomLogoutEndpointApplication.java
@@ -10,4 +10,46 @@ public class CustomLogoutEndpointApplication {
 		SpringApplication.run(CustomLogoutEndpointApplication.class, args);
 	}
 
+	/**
+	 * OpenID Connect 1.0.. 용어 정리.
+	 * OP (OpenID Provider)
+	 * 		사용자의 인증을 수행하고, ID Token 등을 발급함
+	 * 		-> authorization server
+	 * RP (Relying Party)
+	 * 		OP로부터 인증받고, ID Token을 통해 사용자 정보를 신뢰함
+	 * 		-> client
+	 */
+
+	/**
+	 * 여러 구현 상황을 보며..
+	 * RP-Initiated Logout 과 Front-channel Logout, Back-channel Logout 에 대해 알아본다.
+	 *
+	 * 사전조건
+	 * client 가 a.com, b.com, c.com 이 있고 authorization server 는 auth.com 이다.
+	 * 모든 client 에서 sso 로 auth.com 세션을 재사용 하면서 로그인을 했다.
+	 *
+	 * 1. client 들과 OAuth2.0 authorization server 의 logout 과정..
+	 * 어떤 client 에서 logout 하면.. 해당 client 에서의 세션(RP 세션)이 종료되고 해당 client 에 발급된 token revoke 시키고 끝난다.
+	 * OAuth 2.0 의 authorization server 에는 세션이 없다.(세션이 있을 수 도 있는데 표준 외이다.)
+	 *
+	 * 2. front-channel, back-channel 구현하지 않은 그냥 oidc authorization server 와 client 들의 logout 과정
+	 * a.com 에서 로그아웃을 하면 a.com 의 자체 세션(RP 세션)이 종료되고 a.com 에 발급된 token revoke 시킨다.
+	 * 여기서부터는 선택사항,
+	 * a.com 에서 authorization server 가 제공하는 logout url 로 리다이렉트 시킬수도 있고 그대로 그냥 끝날 수도 있다.
+	 * 만약 리다이렉트를 시킨다면 auth.com 에서 OP 세션을 종료 시킴과 동시에 sso 는 끝날것이다..
+	 * 그리고 b.com, c.com 은 로그인이 유지 될 것이다. (b, c client 의 세션 및 token 에 영향이 없음)
+	 *
+	 * 3. front-channel logout 구현, oidc authorization server 와 client 들의 logout 과정
+	 * a.com 에서 로그아웃을 하면 a.com 의 자체 세션(RP 세션)이 종료되고 a.com 에 발급된 token revoke 시킨다.
+	 * 그리고 a.com 에서 authorization server 가 제공하는 logout url 로 리다이렉트 시킨다.
+	 * auth.com 에서 OP 세션을 종료 시킴과 동시에 sso 는 끝날것이다..
+	 * 그리고 사용자의 웹브라우저가 b.com, c.com 으로 logout 을 요청하도록 명령이 되어있는 응답을 보낸다.(iframe 이용)
+	 *
+	 * 4. backchannel logout 구현, oidc authorization server 와 client 들의 logout 과정
+	 * a.com 에서 로그아웃을 하면 a.com 의 자체 세션(RP 세션)이 종료되고 a.com 에 발급된 token revoke 시킨다.
+	 * 그리고 a.com 에서 authorization server 가 제공하는 logout url 로 리다이렉트 시킨다.
+	 * auth.com 에서 OP 세션을 종료 시킴과 동시에 sso 는 끝날것이다..
+	 * 그리고 auth.com이 b.com, c.com 으로 logout 직접 요청을 보내고 사용자 브라우저로 응답보내고 끝난다. (직접 server to server 요청)
+	 */
+
 }
