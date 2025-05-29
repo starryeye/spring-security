90ba13a240e01cbf8389988132c322e249d75751
starryeye <see2818@naver.com>
2025-05-29 21:31:58 +0900

    [starryeye, 2025.05.29] - OAuth 2.0 OAuth2AuthorizationServerConfigurer::oidc, OidcConfigurer::userInfoEndpoint
    내용
    - OAuth2AuthorizationServerConfigurer::oidc, OidcConfigurer::logoutEndpoint

---
+++ oauth-2/authorization-server/oidc/custom-userinfo-endpoint/src/main/java/dev/starryeye/custom_userinfo_endpoint/CustomUserinfoEndpointApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_userinfo_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomUserinfoEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomUserinfoEndpointApplication.class, args);
+	}
+
+}
---
+++ oauth-2/authorization-server/oidc/custom-userinfo-endpoint/src/test/java/dev/starryeye/custom_userinfo_endpoint/CustomUserinfoEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.custom_userinfo_endpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomUserinfoEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
