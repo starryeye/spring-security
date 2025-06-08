57c5d68d078ec861dbc609d82e5ffd1a712b70b5
starryeye <see2818@naver.com>
2025-06-08 16:40:32 +0900

    [starryeye, 2025.06.08] - practice, client-server and resource-server and authorization-server
    내용
    - practice, client-server and resource-server and authorization-server

--- oauth-2/practices/simple-integration-client-and-resource-server-and-authorization-server/resource-server-article/src/main/java/dev/starryeye/resource_server_article/exception_handler/GlobalExceptionHandler.java
+++ oauth-2/practices/simple-integration-client-and-resource-server-and-authorization-server/resource-server-article/src/main/java/dev/starryeye/resource_server_article/exception_handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package dev.starryeye.resource_server_article.exception_handler;
 
 import dev.starryeye.resource_server_article.api.service.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,12 +9,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> handleNotFound(NotFoundException ex) {
+        log.error(ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
+        log.error(ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Map.of("message", ex.getMessage()));
     }
 }
